7eb85c4bc21b350201d6993bfc73c4328ab79035
Edi <ediprekpalaj78@gmail.com>
2023-10-02 20:45:45 -0400

    Finished fixing up google login

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,24 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route }
 	from 'react-router-dom';
 import Feed from './pages/feed';
+import {GoogleLogin } from '@react-oauth/google';
 import Profile from './pages/profile';
 
 function App() {
+	const responseMessage = (response) => {
+        console.log(response);
+    };
+    const errorMessage = (error) => {
+        console.log(error);
+    };
 	return (
 		<Router>
+			<div>
+            	<h2>React Google Login</h2>
+            	<br />
+            	<br />
+            	<GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+        	</div>
 			<Navbar />
 			<Routes>
 				<Route exact path='/feed' element={<Feed />} />
