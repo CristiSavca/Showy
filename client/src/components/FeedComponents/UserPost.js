bd12caa6daa4ff049e2b5cda0faf84a4e29daebc
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-06 10:44:49 -0400

    moving user post to project

---
+++ client/src/components/FeedComponents/UserPost.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+export default function UserPost({postId, username, header, postText, likes}) {
+    const [likedPost, setLikedPost] = useState(false);
+    const [totalLikes, setTotalLikes] = useState(likes);
+
+    function changeLikes() {
+      if (!likedPost) { // console.log shows opposite of totalLikes compared to rendered need to check that out
+        setLikedPost(true);
+        setTotalLikes(totalLikes + 1);
+      } else {
+        setLikedPost(false);
+        setTotalLikes(totalLikes - 1);
+      }
+    }
+
+    return (
+      <div className="post-box">
+        <p>{username}</p>
+        <Link to={`/posts/${postId}`}><h2>{header}</h2></Link>
+        <div className="post-content">
+            <p>{postText}</p>
+        </div>
+        <div className="bottom-content">
+            <table>
+              <thead></thead>
+              <tbody>
+                <tr>
+                <th><button onClick={() => changeLikes()}>like {totalLikes}</button></th>
+                <th><button>comment</button></th>
+                </tr>
+              </tbody>
+            </table>
+        </div>
+      </div>
+    )
+}
