0dd78cbc114a03e7e5e4f98c271ce65c23ad1a81
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-06 11:53:17 -0400

    temp use of fixed data to confirm components work

--- client/src/pages/feed.js
+++ client/src/pages/feed.js
@@ -3,7 +3,33 @@ import FeedPosts from "../components/FeedComponents/FeedPosts";
 import useFetchData from "../components/FeedComponents/useFetchData";
 
 const Feed = () => {
-    const {backendData, errorMessage} = useFetchData("/posts");
+    //const {backendData, errorMessage} = useFetchData("/posts");
+
+    const backendData = {
+        "posts": [
+            {
+                "key": "Person-What-is-the-future?",
+                "username": "Person",
+                "header": "What is the future?",
+                "postText": "AI", 
+                "likes": 0
+            },
+            {
+                "key": "Person2-Dogs-are-good-pets", 
+                "username": "Person2", 
+                "header": "Dogs are good pets", 
+                "postText": "Source? me", 
+                "likes": 3
+            },
+            {
+                "key": "Person3-Test", 
+                "username": "Person3", 
+                "header": "Test", 
+                "postText": "123", 
+                "likes": 20
+            }
+        ]
+    };
 
     return (
         <div>
