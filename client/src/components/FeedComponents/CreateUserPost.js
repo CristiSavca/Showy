a7d65fd8166edc290b9d07a90307d6e3d0423729
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-06 15:42:17 -0400

    changed format of function, commented fetch POST

--- client/src/components/FeedComponents/CreateUserPost.js
+++ client/src/components/FeedComponents/CreateUserPost.js
@@ -1,15 +1,11 @@
-import React, {useState} from 'react';
+import { useState } from 'react';
 
-export default function CreateUserPost() {
-    
+const CreateUserPost = () => {
     const [username, setUsername] = useState("");
     const [header, setHeader] = useState("");
     const [userText, setUserText] = useState("");
-    const [userLikes, setUserLikes] = useState(0);
     
     function handlePost() {
-        console.log("handling post");
-        
         const createdPost = {
             "key": username + header,
             "username": username,
@@ -18,34 +14,40 @@ export default function CreateUserPost() {
             "likes": 0
         }
     
-        fetch("/posts", {
-          method: "POST",
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify(createdPost)
-        }).then(
-          response => response.json()
-        ).then(
-          data => {
-            console.log("new", data);
-            //setBackendData(data);
-          }
-        )
+        // fetch("/posts", {
+        //   method: "POST",
+        //   headers: {"Content-Type": "application/json"},
+        //   body: JSON.stringify(createdPost)
+        // }).then(
+        //   response => response.json()
+        // ).then(
+        //   data => {
+        //     console.log("new", data);
+        //     //setBackendData(data);
+        //   }
+        // )
+        console.log(createdPost);
     }
     
     return (
         <div className="post-box">
-            <label>Username:</label> <br />
-            <textarea className="username" value={username} onChange={e => setUsername(e.target.value)}></textarea>
+            <label>Username:</label> 
             <br />
-            <label>Header:</label> <br />
-            <textarea className="header" value={header} onChange={e => setHeader(e.target.value)}></textarea>
+            <textarea className="username" value={username} onChange={e => setUsername(e.target.value)} />
+            <br />
+            <label>Header:</label> 
+            <br />
+            <textarea className="header" value={header} onChange={e => setHeader(e.target.value)} />
             <div className="post-content">
-                <label>Text:</label> <br />
-                <textarea className="userText" value={userText} onChange={e => setUserText(e.target.value)}></textarea>
+                <label>Text:</label> 
+                <br />
+                <textarea className="userText" value={userText} onChange={e => setUserText(e.target.value)} />
             </div>
             <div className="bottom-content">
-                    <button onClick={() => handlePost()}>Post</button>
+                <button onClick={() => handlePost()}>Post</button>
             </div>
         </div>
     )
 }
+
+export default CreateUserPost;
