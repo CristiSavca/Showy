99b8d25e79c5bc20b7aaa8088b1f4921f3a7c366
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-06 10:50:43 -0400

    put component into feed page

--- client/src/pages/feed.js
+++ client/src/pages/feed.js
@@ -1,11 +1,15 @@
 import React from "react";
  
 const Feed = () => {
+    const {backendData, errorMessage} = useFetchData("/posts");
+
     return (
         <div>
             <h1>
                 Posts & stuff...
             </h1>
+
+            <Feed backendData={backendData} />
         </div>
     );
 };
