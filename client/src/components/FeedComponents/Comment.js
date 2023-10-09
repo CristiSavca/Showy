2e4d8bc46b11c1241f2692ca6df6211a4dacfcdf
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-09 17:27:55 -0400

    allow comment replies to show up on comments

--- client/src/components/FeedComponents/Comment.js
+++ client/src/components/FeedComponents/Comment.js
@@ -1,11 +1,15 @@
+import CommentsDisplay from "./CommentsDisplay";
 
-const Comment = ({username, commentText, likes, commentId}) => {
+const Comment = ({username, commentText, likes, commentId, replies}) => {
     return (
         <div className="comment">
             <p>{commentId}</p>
             <p>{username}</p>
             <p>{commentText}</p>
             <p>{likes}</p>
+            <div>
+                <CommentsDisplay commentsData={replies}/>
+            </div>
         </div>
     )
 }
