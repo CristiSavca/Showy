a8d96c8f9591cf6a18cdf2813d55386c3fafd9e3
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-09 18:10:54 -0400

    added fixed comment data to test comment display, adding comments to post not working

--- client/src/components/FeedComponents/WholeUserPost.js
+++ client/src/components/FeedComponents/WholeUserPost.js
@@ -2,29 +2,84 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import useFetchData from './useFetchData';
 import UserPost from "./UserPost";
+import CommentsDisplay from "./CommentsDisplay";
 
 const WholeUserPost = () => {
     // const {id} = useParams();
 
     // const {backendData, errorMessage} = useFetchData("/posts/" + id);
-
-    const backendData = {
-
-            "key": "Person2-Dogs-are-good-pets", 
-            "username": "Person2", 
-            "header": "Dogs are good pets", 
-            "postText": "Source? me", 
-            "likes": 3
-
-    }
-    
     const [count, setCount] = useState(0);
     const [saveComment, setComment] = useState("");
     const [textContent, setTextContent] = useState("");
-  
-    function createNewComment(textContent) {
-      setComment(textContent);
+
+    const backendData = {
+      "key": "Person2-Dogs-are-good-pets", 
+      "username": "Person2", 
+      "header": "Dogs are good pets", 
+      "postText": "Source? me", 
+      "likes": 3
     }
+
+    const commentsData = [
+      {
+        "username": "Person2",
+        "commentText": "Hello everyone",
+        "likes": 0,
+        "replies": [
+          {
+            "username": "Person1",
+            "commentText": "This is a reply",
+            "likes": 0,
+            "replies": [
+              {
+                "username": "Person98989",
+                "commentText": "This is a reply to a reply",
+                "likes": 0,
+                "replies": []
+              }
+            ]
+          },
+          {
+            "username": "Person8",
+            "commentText": "This is a reply112112",
+            "likes": 0,
+            "replies": []
+          },
+          {
+            "username": "Person1",
+            "commentText": "This is a reply 9898989 ",
+            "likes": 0,
+            "replies": []
+          }
+        ]
+      },
+      {
+        "username": "Person3",
+        "commentText": "Hello world",
+        "likes": 0,
+        "replies": []
+      }
+    ]
+  
+
+
+    function createNewComment(textContent) {
+      commentsData.push({
+        "username": backendData.username,
+        "commentText": textContent,
+        "likes": 0,
+        "replies": []
+      })
+    }
+
+
+    // useEffect(() => {
+    // }, [commentsData]);
+
+    function eraseNewComment(textContent) {
+      setTextContent("");
+    }
+
   
     return (
       <div className="whole-post-box">
@@ -32,13 +87,20 @@ const WholeUserPost = () => {
                     username={backendData.username} 
                         header={backendData.header} 
                             postText={backendData.postText} 
-                                likes={backendData.likes} postId={backendData.key}/>
+                                likes={backendData.likes} 
+                                    postId={backendData.key}/>
 
         {/* {console.log(id)}; */}
         <div className="whole-post-comment-box">
           <textarea className="whole-post-comment-textarea" value={textContent} onChange={e => setTextContent(e.target.value)}></textarea>
           <button onClick={() => createNewComment(textContent)}>Comment</button>
-          <p>{saveComment}</p>
+          {/* remember to figure out why erasing the textarea resets the array of data given (meaning that it resets all user input in, and only leaves fixed data behind) */}
+          {/* should be fixed if adding to database properly*/}
+          <button onClick={() => eraseNewComment(textContent)}>Erase</button>
+          {/* <p>{saveComment}</p> */}
+        </div>
+        <div>
+          <CommentsDisplay commentsData={commentsData}/>
         </div>
       </div>
     )
