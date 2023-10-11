a7ef9318078caf219faac9096e3ee18c767abe6f
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-11 18:59:16 -0400

    isolating like button

---
+++ client/src/components/FeedComponents/LikeButton.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+const LikeButton = ({currentLikes}) => { // props should be the amount of likes for the post / comment and if it was liked before
+    const [likedObject, setLikedObject] = useState(false); // todo use props for liked object
+    const [totalLikes, setLike] = useState(currentLikes); // todo use props for likes
+
+    function clickLike() { // need to consider saving who liked the post save post id that was liked and liked state
+        if (likedObject) { // console.log shows opposite of totalLikes compared to rendered need to check that out
+            setLike(totalLikes - 1);
+            setLikedObject(false);
+        } else if (!likedObject) {
+            setLike(totalLikes + 1);
+            setLikedObject(true);
+        }
+    }
+    return (
+        <div>
+            <button onClick={() => {clickLike()}}>Like {totalLikes}</button>
+        </div>
+    )
+}
+
+export default LikeButton;
