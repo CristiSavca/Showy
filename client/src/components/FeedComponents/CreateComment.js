20af70d73e54367b2b6b93dac2307d2308ee4107
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-11 18:59:44 -0400

    create comment will be separate component

--- client/src/components/FeedComponents/CreateComment.js
+++ client/src/components/FeedComponents/CreateComment.js
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 
 const CreateComment = () => {
-    
+    const [comment, setComment] = useState("");
+
+    function saveCommentContent(comment) {
+    }
+
+    return (
+        <div>
+            {/* <textarea className="whole-post-comment-textarea" value={comment} onChange={e => setComment(e.target.value)}></textarea>
+            <button></button> */}
+        </div>
+    )
 }
 
 export default CreateComment;
