9526d25128c82423227d98744c0ab9252ea15f3a
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-24 14:05:32 -0400

    remove useFetchData from code

--- client/src/components/FeedComponents/WholeUserPost.js
+++ client/src/components/FeedComponents/WholeUserPost.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 // import { useParams } from 'react-router-dom';
-import useFetchData from './useFetchData';
+
 import UserPost from "./UserPost";
 import CommentsDisplay from "./CommentsDisplay";
 
