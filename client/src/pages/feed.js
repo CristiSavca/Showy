c499acc45825d3404be8f3dc84777faedbd21d7c
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-24 14:10:07 -0400

    also remove import of useFetchData from feed.js

--- client/src/pages/feed.js
+++ client/src/pages/feed.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 import FeedPosts from "../components/FeedComponents/FeedPosts";
-import useFetchData from "../components/FeedComponents/useFetchData";
 import CreateUserPost from "../components/FeedComponents/CreateUserPost";
 
 const Feed = () => {
