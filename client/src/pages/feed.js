3bd0147dcc3e7cfe404ea1402a36686b17034428
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-30 16:47:42 -0400

    Modified index, feed, and profile from 271b255

--- client/src/pages/feed.js
+++ client/src/pages/feed.js
@@ -1,11 +1,61 @@
+// import { collection, getDocs } from "firebase/firestore";
+// import { db } from '../db';
+
 import React from "react";
- 
+import { Link } from 'react-router-dom';
+
+import FeedPosts from "../components/FeedComponents/FeedPosts";
+
 const Feed = () => {
+    // const [postsData, setPostsData] = useState([]);
+
+    async function getPostsData() {
+        // await getDocs(collection(db, "posts"))
+        //     .then((querySnapshot)=>{               
+        //         const collectedPosts = querySnapshot.docs.map((doc) => ({...doc.data(), id:doc.id }));
+        //         setPostsData(collectedPosts);       
+        //     })
+    }
+
+    // useEffect(() => {
+    //     getPostsData();
+    // }, [postsData]);
+
+    const postsData = { // REMOVE THIS WHEN DATABASE FUNCTIONS ARE CONNECTED
+        "posts": [
+            {
+                "key": "Person-What-is-the-future?",
+                "username": "Person",
+                "header": "What is the future?",
+                "postText": "AI", 
+                "likes": 0
+            },
+            {
+                "key": "Person2-Dogs-are-good-pets", 
+                "username": "Person2", 
+                "header": "Dogs are good pets", 
+                "postText": "Source? me", 
+                "likes": 3
+            },
+            {
+                "key": "Person3-Test", 
+                "username": "Person3", 
+                "header": "Test", 
+                "postText": "123", 
+                "likes": 20
+            }
+        ]
+    };
+
     return (
         <div>
             <h1>
                 Posts & stuff...
             </h1>
+            <Link to={`/posts/create-post`}>
+                <button>Create post</button>
+            </Link>
+            <FeedPosts postsData={postsData.posts} />
         </div>
     );
 };
--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,9 +1,50 @@
-import React from 'react';
- 
-const Profile = () => {
+import React, { useEffect, useState } from 'react';
+import { useAuth, upload } from '../firebase';
+import ActivityTracker from '../components/contributions';
+
+export default function Profile() {
+    // State from the second block
+    const currentUser = useAuth();
+    const [photo, setPhoto] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    // Photo URL logic from the second block
+    const defaultPhotoURL = currentUser?.photoURL || "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png";
+    const [photoURL, setPhotoURL] = useState(defaultPhotoURL);
+
+    // Handlers from the second block
+    function handleChange(e) {
+        if (e.target.files[0]) {
+            setPhoto(e.target.files[0]);
+        }
+    }
+
+    async function handleClick() {
+        const newPhotoURL = await upload(photo, currentUser, setLoading);
+        if (newPhotoURL) {
+            setPhotoURL(newPhotoURL);
+        }
+    }
+
+    // Effect from the second block
+    useEffect(() => {
+        if (currentUser?.photoURL) {
+            setPhotoURL(currentUser.photoURL);
+        }
+    }, [currentUser]);
+
     return (
-        <h1>Profile loading...</h1>
+        <div>
+            <h1>Profile Page</h1>
+            {/* Picture Upload */}
+            <div className="fields">
+                <input type="file" onChange={handleChange} />
+                <button disabled={loading || !photo} onClick={handleClick}>Upload</button>
+                <img src={photoURL} alt="Avatar" className="avatar" />
+            </div>
+
+            {/* Contribution Graph */}
+            <ActivityTracker />
+        </div>
     );
-};
- 
-export default Profile;
+}
