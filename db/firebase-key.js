e36b18da9f3c00afbc0e0823fe57544226be643c
Jesse Han <jhan3537@bths.edu>
2023-11-07 10:02:04 -0500

    Obscured some other env variables

--- db/firebase-key.js
+++ db/firebase-key.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 export const firebaseKey = {
   "type": "service_account",
-  "project_id": "showy-92cc7",
+  "project_id": process.env.project_id,
   "private_key_id": process.env.private_key_id,
   "private_key": process.env.private_key,
   "client_email": process.env.client_email,
@@ -14,8 +14,8 @@ export const firebaseKey = {
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-g3h0s%40showy-92cc7.iam.gserviceaccount.com",
+  "client_x509_cert_url": process.env.client_x509_cert_url,
   "universe_domain": "googleapis.com"
 };
 
-export const firebaseURL = "https://showy-92cc7-default-rtdb.firebaseio.com/";
+export const firebaseURL = process.env.firebase_url;
