22ef694131232c9d4fb517d079385640b0d93e0c
Jesse Han <jhan3537@bths.edu>
2023-11-06 23:55:09 -0500

    Added environment variables, provided separately

--- db/db-test.js
+++ db/db-test.js
@@ -5,11 +5,28 @@
 
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-import Database from "./db.js"
+
 
 const prompt = require('prompt-sync')();
 
-const svcAccount = require("D:/Downloads/showy-92cc7-firebase-adminsdk-g3h0s-dc80e19b7c.json");
+import Database from "./db.js"
+
+// REQUIRES .env
+require('dotenv').config();
+var firebaseCreds = {
+  "type": "service_account",
+  "project_id": "showy-92cc7",
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_email": process.env.client_email,
+  "client_id": process.env.client_id,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-g3h0s%40showy-92cc7.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+};
+
 const dbURL = "https://showy-92cc7-default-rtdb.firebaseio.com/";
 
 // -------------- TESTING BELOW -----------
@@ -17,7 +34,7 @@ const dbURL = "https://showy-92cc7-default-rtdb.firebaseio.com/";
 var user = "";
 var currentId = "";
 
-const dbTest = new Database(svcAccount, dbURL);
+const dbTest = new Database(firebaseCreds, dbURL);
 
 var repeat = true;
 while (repeat) {
