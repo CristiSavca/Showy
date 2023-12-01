a01cc404a4b2602505158eae0e1eb7edf336ddb6
steph-kimm <stephaniekim40@gmail.com>
2023-12-01 17:13:34 -0500

    made all components moveable

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -20,56 +20,33 @@ export default function Profile() {
         console.log(isEditing)
     }
 
-    const example_data = [
-        {component: <>hello</>, 
-        location:   {x: 0, y: 0}
-    }
-    ]
-    // const [photo, setPhoto] = useState(null);
-    // const [loading, setLoading] = useState(false);
 
-    // Photo URL logic from the second block
-    // const defaultPhotoURL = currentUser?.photoURL || "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png";
-    // const [photoURL, setPhotoURL] = useState(defaultPhotoURL);
-
-    // Handlers from the second block
-    // function handleChange(e) {
-    //     if (e.target.files[0]) {
-    //         setPhoto(e.target.files[0]);
-    //     }
-    // }
-
-    // async function handleClick() {
-    //     const newPhotoURL = await upload(photo, currentUser, setLoading);
-    //     if (newPhotoURL) {
-    //         setPhotoURL(newPhotoURL);
-    //     }
-    // }
-
+    console.log( 'currentUser',currentUser)
     // Effect from the second block
     // useEffect(() => {
-    //     if (currentUser?.photoURL) {
-    //         setPhotoURL(currentUser.photoURL);
-    //     }
-    // }, [currentUser]);
 
+    // });
+    const example_data = [
+        {component: <>hello</>, 
+        location:   {x: 0, y: 0}}, 
+        {component: <>hello</>, 
+        location:   {x: 0, y: 0}}, 
+        {component: <ProfilePic/>, 
+        location:   {x: 0, y: 0}}, 
+        {component: <ActivityTracker/>, 
+        location:   {x: 0, y: 0}}, 
+    ]
     return (
         <div>
             <h1>Profile Page</h1>
             {/* Picture Upload */}
 
-            {/* <div className="fields">
-                <input type="file" onChange={handleChange} />
-                <button disabled={loading || !photo} onClick={handleClick}>Upload</button>
-                <img src={photoURL} alt="Avatar" className="avatar" />
-            </div> */}
-
             {/* Contribution Graph */}
-            <h1>Name here</h1>
+            <h1> {currentUser.email}'s Account</h1>
             <button onClick={()=> toggleEditing()}> {buttonText}</button>
             {example_data.map((item)=> <ProfileContainer disabled={!isEditing} defaultPosition={item.location} component={item.component}/>)}
-            <ProfilePic/>
-            <ActivityTracker />
+            {/* <ProfilePic/> */}
+            {/* <ActivityTracker /> */}
         </div>
     );
 }
