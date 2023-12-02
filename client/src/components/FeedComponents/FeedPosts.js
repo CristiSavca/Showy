95349345f4f029275e3c34bcc2d452210f932a4f
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-12-01 21:58:51 -0500

    moved userNameId from like button to feed posts

--- client/src/components/FeedComponents/FeedPosts.js
+++ client/src/components/FeedComponents/FeedPosts.js
@@ -1,18 +1,23 @@
+import { useSelector } from 'react-redux';
+
 import UserPost from "./UserPost";
 
 const FeedPosts = ({postsData}) => {
-    return (
-        <>      
-          {(typeof postsData === "undefined") ? (<p>Loading...</p>)
-          :(postsData.map((post) => <UserPost key={post.postId}
-                                                  username={post.username} 
-                                                    header={post.header} 
-                                                      postText={post.body} 
-                                                        likes={post.likes} 
-                                                          postId={post.postId} />)
+  const userNameId = useSelector((state) => state.saveUsername.usernameId);
+
+  return (
+    <>      
+      {(typeof postsData === "undefined") ? (<p>Loading...</p>)
+        :(postsData.map((post) => <UserPost key={post.postId}
+                                              username={post.username} 
+                                                header={post.header} 
+                                                  postText={post.body} 
+                                                    likes={post.likes} 
+                                                      postId={post.postId}
+                                                        currentUsername={userNameId} />)
           )}
-        </>
-    )
+    </>
+  )
 }
 
 export default FeedPosts;
