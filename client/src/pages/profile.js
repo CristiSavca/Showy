f26058ac21eb68e93e9bd033608c134f16f7e83d
steph-kimm <stephaniekim40@gmail.com>
2023-12-05 14:23:24 -0500

    fixed bug

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,4 +1,6 @@
 import Axios from 'axios';
+// import { jsonc } from 'jsonc';
+import { parse, stringify} from 'flatted';
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import { useAuth } from '../firebase';
@@ -11,35 +13,64 @@ export default function Profile() {
     const currentUser = useAuth();
     const [username, setUsername] = useState(null);
     const [isEditing, setisEditing] = useState(false);
-    const [customization, setCustomization] = useState([]);
+    const [customization, setCustomization] = useState([{
+        component: <ProfilePic />,
+        location: { x: 0, y: 0 }
+    },
+    {
+        component: <ActivityTracker />,
+        location: { x: 0, y: 0 }
+    },]);
     const [buttonText, setButtonText] = useState('Press me to Enable Editing');
     console.log(currentUser);
 
     // const componentLocations = getUserCustomizations(currentUser?.uid) //|| "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png";
     async function changeCustomization() {
         let stringCustomizations = [];
-        for(let i = 0; i < customization.length(); i++){
-            stringCustomizations.push(JSON.stringify(customization[i]));
+        for (let i = 0; i < customization.length; i++) {
+            console.log('customization[i]: ', customization[i].component, typeof customization[i].component)
+            // stringCustomizations.push(jsonc.stringify(customization[i]));
+            console.log('customization[i]', customization[i])
+            console.log({
+                component: <ActivityTracker />,
+                location: { x: 0, y: 0 }
+            })
+            const item = customization[i];
+            console.log('stringify(item): ', stringify(item), typeof stringify(item))
+            stringCustomizations.push(stringify(item));
+            
+            // stringCustomizations.push(JSON.stringify({
+            //     component: <ActivityTracker />,
+            //     location: { x: 0, y: 0 }
+            // }));
+            // Flatted.parse(Flatted.stringify(data))
+            // stringCustomizations.push(stringify(customization[i]))
+            // console.log("STRINGIFY", stringify(customization[i]))
         }
         await Axios.patch('http://localhost:5000/changeCustomizations', {
             uuid: currentUser.uid,
             customizations: stringCustomizations,
         })
-        .then((response) => {
-            console.log(response);
-        }).catch((error) => {
+            .then((response) => {
+                console.log(response);
+            }).catch((error) => {
                 console.log(error);
-        });
+            });
     }
 
-  function toggleEditing(){
+    function toggleEditing() {
+        // console.log(isEditing, customization)
         setisEditing(!isEditing)
-        if(!isEditing){ 
+        console.log(isEditing, customization)
+        if (!isEditing) {
             setButtonText("Press me to Disable Editing");
+            // changeCustomization();
+        }
+        else {
+            setButtonText("Press me to Enable Editing")
             changeCustomization();
         }
-        else setButtonText("Press me to Enable Editing")
-        console.log(isEditing)
+        // console.log(isEditing)
     }
 
 
@@ -54,7 +85,7 @@ export default function Profile() {
                 }
             }).then((response) => {
                 setUsername(response.data);
-                console.log("Name: ",response.data);
+                console.log("Name: ", response.data);
             }).catch((error) => {
                 console.log(error);
             });
@@ -68,11 +99,12 @@ export default function Profile() {
             }).then((response) => {
                 //parse the array back to objects from strings
                 let parsed_array = [];
-                for(let i = 0; i < response.data.length(); i++){
-                    parsed_array.push(JSON.parse(response.data[i]));
+                for (let i = 0; i < response.data.length; i++) {
+                    // parsed_array.push(jsonc.parse(response.data[i]));
+                    parsed_array.push(parse(response.data[i]));
                 }
                 setCustomization(parsed_array);
-                console.log("customizations: ",response.data);
+                console.log("customizations: ", response.data);
             }).catch((error) => {
                 console.log(error);
             });
@@ -81,18 +113,22 @@ export default function Profile() {
         getCustomization();
     }, [currentUser]);
 
-    let example_data = [
-        // {component: <h1>{currentUser.email}'s Account</h1>, 
-        // location:   {x: 0, y: 0}}, 
-        {component: <ProfilePic/>, 
-        location:   {x: 0, y: 0}}, 
-        {component: <ActivityTracker/>, 
-        location:   {x: 0, y: 0}}, 
-    ]
-    const setLocation = (index, new_location) =>{
+    // let example_data = [
+    //     // {component: <h1>{currentUser.email}'s Account</h1>, 
+    //     // location:   {x: 0, y: 0}}, 
+    //     {
+    //         component: <ProfilePic />,
+    //         location: { x: 0, y: 0 }
+    //     },
+    //     {
+    //         component: <ActivityTracker />,
+    //         location: { x: 0, y: 0 }
+    //     },
+    // ]
+    const setLocation = (index, new_location) => {
         // delete bottom two lines after connect to DB
-        example_data[index].location.x = new_location.lastX;
-        example_data[index].location.y = new_location.lastY;
+        // example_data[index].location.x = new_location.lastX;
+        // example_data[index].location.y = new_location.lastY;
 
         // keep below
         let data = customization;
@@ -100,7 +136,7 @@ export default function Profile() {
         data[index].location.y = new_location.lastY;
 
         setCustomization(data)
-        console.log(example_data);
+        // console.log(example_data);
     }
     return (
         <div>
@@ -108,11 +144,11 @@ export default function Profile() {
             {/* Picture Upload */}
 
             {/* Contribution Graph */}
-            {currentUser?.email ? <h1> {username? username: currentUser.email}'s Account</h1> : null}
-            <button onClick={()=> toggleEditing()}> {buttonText}</button>
+            {currentUser?.email ? <h1> {username ? username : currentUser.email}'s Account</h1> : null}
+            <button onClick={() => toggleEditing()}> {buttonText}</button>
             {/* delete example data line below after connected  */}
-            {example_data.map((item, index)=> <ProfileContainer parentCallback={setLocation} index={index}disabled={!isEditing} defaultPosition={item.location} component={item.component}/>)}
-            {customization.map((item, index)=> <ProfileContainer parentCallback={setLocation} index={index}disabled={!isEditing} defaultPosition={item.location} component={item.component}/>)}
+            {/* {example_data.map((item, index) => <ProfileContainer parentCallback={setLocation} index={index} disabled={!isEditing} defaultPosition={item.location} component={item.component} />)} */}
+            {customization.map((item, index) => <ProfileContainer parentCallback={setLocation} index={index} disabled={!isEditing} defaultPosition={item.location} component={item.component} />)}
             {/* <ProfilePic/> */}
             {/* <ActivityTracker /> */}
         </div>
