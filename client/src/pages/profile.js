947b1373e5cba4f6572169be5d385a9fc969bbb5
steph-kimm <stephaniekim40@gmail.com>
2023-12-05 13:12:07 -0500

    added customizations to database

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -11,12 +11,27 @@ export default function Profile() {
     const currentUser = useAuth();
     const [username, setUsername] = useState(null);
     const [isEditing, setisEditing] = useState(false);
-    const [customization, setCustomization] = useState(false);
+    const [customization, setCustomization] = useState([]);
     const [buttonText, setButtonText] = useState('Press me to Enable Editing');
     console.log(currentUser);
 
     // const componentLocations = getUserCustomizations(currentUser?.uid) //|| "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png";
-  
+    async function changeCustomization() {
+        let stringCustomizations = [];
+        for(let i = 0; i < customization.length(); i++){
+            stringCustomizations.push(JSON.stringify(customization[i]));
+        }
+        await Axios.patch('http://localhost:5000/changeCustomizations', {
+            uuid: currentUser.uid,
+            customizations: stringCustomizations,
+        })
+        .then((response) => {
+            console.log(response);
+        }).catch((error) => {
+                console.log(error);
+        });
+    }
+
   function toggleEditing(){
         setisEditing(!isEditing)
         if(!isEditing){ 
@@ -31,15 +46,6 @@ export default function Profile() {
     // console.log( 'currentUser',currentUser)
     // Effect from the second block
     useEffect(() => {
-        Axios.interceptors.request.use(function (config) {
-            // Do something before request is sent
-            console.log('config: ', config)
-            return config;
-          }, function (error) {
-            // Do something with request error
-            return Promise.reject(error);
-          });
-
         console.log("ID: ", currentUser?.uid)
         async function getUsername() {
             await Axios.get("http://localhost:5000/getUsername", {
@@ -60,16 +66,17 @@ export default function Profile() {
                     uid: currentUser?.uid
                 }
             }).then((response) => {
+                //parse the array back to objects from strings
+                let parsed_array = [];
                 for(let i = 0; i < response.data.length(); i++){
-                    
+                    parsed_array.push(JSON.parse(response.data[i]));
                 }
-                setCustomization(response.data);
+                setCustomization(parsed_array);
                 console.log("customizations: ",response.data);
             }).catch((error) => {
                 console.log(error);
             });
         }
-
         getUsername();
         getCustomization();
     }, [currentUser]);
@@ -83,8 +90,16 @@ export default function Profile() {
         location:   {x: 0, y: 0}}, 
     ]
     const setLocation = (index, new_location) =>{
+        // delete bottom two lines after connect to DB
         example_data[index].location.x = new_location.lastX;
         example_data[index].location.y = new_location.lastY;
+
+        // keep below
+        let data = customization;
+        data[index].location.x = new_location.lastX;
+        data[index].location.y = new_location.lastY;
+
+        setCustomization(data)
         console.log(example_data);
     }
     return (
@@ -95,7 +110,9 @@ export default function Profile() {
             {/* Contribution Graph */}
             {currentUser?.email ? <h1> {username? username: currentUser.email}'s Account</h1> : null}
             <button onClick={()=> toggleEditing()}> {buttonText}</button>
+            {/* delete example data line below after connected  */}
             {example_data.map((item, index)=> <ProfileContainer parentCallback={setLocation} index={index}disabled={!isEditing} defaultPosition={item.location} component={item.component}/>)}
+            {customization.map((item, index)=> <ProfileContainer parentCallback={setLocation} index={index}disabled={!isEditing} defaultPosition={item.location} component={item.component}/>)}
             {/* <ProfilePic/> */}
             {/* <ActivityTracker /> */}
         </div>
