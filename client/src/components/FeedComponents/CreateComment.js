9729d99af02412a99b2e4511b183d30fddfed58a
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-12-06 16:02:45 -0500

    make sure user logs in to comment

--- client/src/components/FeedComponents/CreateComment.js
+++ client/src/components/FeedComponents/CreateComment.js
@@ -5,7 +5,7 @@ const CreateComment = ({userNameId, postId}) => {
     const [username, setUsername] = useState();
     const [comment, setComment] = useState("");
     const [headerComment, setHeaderComment] = useState();
-    const [createdComment, setCreatedComment] = useState(false);
+    //const [createdComment, setCreatedComment] = useState(false);
 
     useEffect(() => {
         async function getUsername() {
@@ -27,23 +27,27 @@ const CreateComment = ({userNameId, postId}) => {
 
 
     function checkComment() {
+        if (username === '') {
+            alert("Log in first");
+            return;
+        }
         if (headerComment === "" || comment === "") {
             alert("Please fill out the comment first.");
-        } else {
-            createComment();
+            return;
         }
+        
+        createComment();
     }
 
 
     async function createComment() {
-        console.log(userNameId);
         await Axios.post('http://localhost:5000/createComment', {
             postId: postId,
             posterId: username,
             title: headerComment,
             body: comment,
         }).then((response) => {
-            setCreatedComment(response.data);
+            //setCreatedComment(response.data);
             console.log(response.status);
         }).catch((error) => {
             console.log(error);
@@ -54,7 +58,7 @@ const CreateComment = ({userNameId, postId}) => {
         <>
             <textarea value={headerComment} onChange={e => setHeaderComment(e.target.value)} placeholder="Header" required></textarea>
             <textarea className="comment-textarea" value={comment} onChange={e => setComment(e.target.value)} placeholder="Comment here" required></textarea>
-            <button onClick={() => {checkComment()}}>comment</button>
+            {userNameId === "" || userNameId === null ? <button onClick={() => {alert("Log in first")}}>log in to comment</button> : <button onClick={() => {checkComment()}}>comment</button>}
         </>
     )
 }
