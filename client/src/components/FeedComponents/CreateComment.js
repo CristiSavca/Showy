dca327ff627a49a717b3887a2d2e5fe204e69748
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-12-06 23:36:12 -0500

    removed /getUsername, clean up code

--- client/src/components/FeedComponents/CreateComment.js
+++ client/src/components/FeedComponents/CreateComment.js
@@ -2,65 +2,49 @@ import { useState, useEffect } from 'react';
 import Axios from 'axios';
 
 const CreateComment = ({userNameId, postId}) => {
-    const [username, setUsername] = useState();
     const [comment, setComment] = useState("");
     const [headerComment, setHeaderComment] = useState("");
-    //const [createdComment, setCreatedComment] = useState(false);
 
     useEffect(() => {
-        async function getUsername() {
-            await Axios.get("http://localhost:5000/getUsername", {
-                params: {
-                    uid: userNameId
-                }
-            }).then((response) => {
-                setUsername(response.data);
-            }).catch((error) => {
-                console.log(error);
-            });
-        }
-
-        if (userNameId !== "") { 
-            getUsername();
-        }
-    }, [userNameId]);
-
+        clearComment();
+    }, []);
 
     function checkComment() {
-        if (username === '') {
-            alert("Log in first");
-            return;
-        }
         if (headerComment === "" || comment === "") {
             alert("Please fill out the comment first.");
             return;
         }
+
+        async function createComment() {
+            await Axios.post('http://localhost:5000/createComment', {
+                postId: postId,
+                posterId: userNameId,
+                title: headerComment,
+                body: comment,
+            }).then((response) => {
+                console.log(response.status);
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
         
         createComment();
+        clearComment();
+    }
+
+    function clearComment() {
         setComment("");
         setHeaderComment("");
     }
 
-
-    async function createComment() {
-        await Axios.post('http://localhost:5000/createComment', {
-            postId: postId,
-            posterId: username,
-            title: headerComment,
-            body: comment,
-        }).then((response) => {
-            //setCreatedComment(response.data);
-            console.log(response.status);
-        }).catch((error) => {
-            console.log(error);
-        });
-    }
-
     return (
         <>
-            <textarea value={headerComment} onChange={e => setHeaderComment(e.target.value)} placeholder="Header" required></textarea>
-            <textarea className="comment-textarea" value={comment} onChange={e => setComment(e.target.value)} placeholder="Comment here" required></textarea>
-            {userNameId === "" || userNameId === null ? <button onClick={() => {alert("Log in first")}}>log in to comment</button> : <button onClick={() => {checkComment()}}>comment</button>}
+            <textarea value={headerComment} onChange={e => setHeaderComment(e.target.value)} placeholder="Header"></textarea>
+            <textarea className="comment-textarea" value={comment} onChange={e => setComment(e.target.value)} placeholder="Body"></textarea>
+            {userNameId === "" || userNameId === null ? 
+                <button onClick={() => {alert("Log in first")}}>log in to comment</button> 
+                : <button onClick={() => {checkComment()}}>comment</button>
+            }
         </>
     )
 }
